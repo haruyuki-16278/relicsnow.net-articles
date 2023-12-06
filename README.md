184710584ebb4e90dfa1a9de54943a2f5b2ca571
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2023-12-06 22:37:32 +0900

    記事リストjsonの生成スクリプトを作成

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+---
+"title": "自己紹介"
+"created_at": "2023/12/1"
+"updated_at": "none"
+"abstract": "ブログ始めるので自己紹介書きました"
+"tags": "自己紹介,読み物"
+---
 ## about me
 
 ![icon](https://avatars.githubusercontent.com/u/24760583?v=4)
---
+++ listing.deno.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env -S deno run --allow-run --allow-write --allow-read
+type Article = {
+  path: string,
+  title: string,
+  created_at: Date,
+  updated_at: Date | undefined,
+  abstract: string,
+  tags: string[]
+}
+
+const filesByte = new Deno.Command('ls').outputSync().stdout
+const files = new TextDecoder().decode(filesByte).split('\n').filter(filename => filename.includes('.md'))
+console.log(files)
+
+const list: Article[] = []
+
+files.forEach(file => {
+  const articleByte = Deno.readFileSync(file)
+  const articleInfo = JSON.parse(`{${new TextDecoder().decode(articleByte).match(/---\n[\s\S\n]+\n---/g)?.[0]?.replace(/\n*---\n*/g, '').replaceAll('\n', ',')}}`)
+  articleInfo.created_at = new Date(articleInfo.created_at)
+  articleInfo.updated_at = articleInfo.updated_at !== 'none' ? new Date(articleInfo.updated_at) : null
+  articleInfo.tags = JSON.parse(`["${articleInfo.tags.replace(',', '","')}"]`)
+  console.log(articleInfo)
+  list.push(articleInfo)
+})
+
+const sorted = list.sort((a, b) => Number(b.created_at) - Number(a.created_at))
+
+Deno.writeFileSync('article-list.json', new TextEncoder().encode(JSON.stringify(sorted)))
